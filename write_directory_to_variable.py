1422461a5e3fd2a692d5033609b17dc0541e9c7c
Walachewaka <nomisbigcow@hotmail.ca>
2022-05-03 21:18:29 -0400

    work

--- write_directory_to_variable.py
+++ write_directory_to_variable.py
@@ -5,11 +5,12 @@ import time
 start_time = time.time()
 
 def write_directory_to_variable():
-    files = glob.glob('Z:' + '/**/*.mp3', recursive=True)
+    files = glob.iglob('Z:' + '/**/*.mp3', recursive=True)
     for file in files:
         global song
         song = file
         #print(song)
+    print(song)
 write_directory_to_variable()
 
 #print("my program took", time.time() - start_time, "seconds to run")
