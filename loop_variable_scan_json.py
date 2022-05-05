cabcc78fb50409548ac3b0495d468f00841a0270
Walachewaka <nomisbigcow@hotmail.ca>
2022-05-05 03:20:06 -0400

    Update loop_variable_scan_json.py

--- loop_variable_scan_json.py
+++ loop_variable_scan_json.py
@@ -1,120 +1,129 @@
 import glob
 import time
 import re
-from mutagen.id3 import ID3
-from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, ID3NoHeaderError, ID3Warning
+from mutagen.mp3 import MP3, HeaderNotFoundError, MutagenError, InvalidMPEGHeader
 import eyed3
+import mutagen
+from mutagen import MutagenError
 from eyed3 import mp3
-#from print_json import write_to_json
 import json
-#import variables
-#from analyze_mp3 import analyze_metadata
-#from print_json import song_json
 
 start_time = time.time()
 
 def write_directory_to_variable():
-    files = glob.iglob('C:/Users/nomis/Music/bethoven remix songs to be edited' + '/**/*.mp3', recursive=True)
+    files = glob.iglob('E:/Music/temp' + '/**/*.mp3', recursive=True)
     for file in files:
-        #global song
         song = file
         audio1 = ID3(song) #artist,album,title,bpm,initial key,date,
         audio2 = MP3(song) #length
-        audio3 = eyed3.load(song) #publisher
+        audio3 = eyed3.load(song) #publisher,genre
         audio4 = mp3.Mp3AudioFile(song) #bitrate
+        artist = ""
+        title = ""
+        album = ""
+        genre = ""
+        bpm = ""
+        initial_key = ""
+        date = ""
+        length = ""
+        publisher = ""
+        bitrate = ""
         file_location = song
+
         try:
             Artist = audio1['TPE1'].text[0]
-            print("Artist:",Artist) #Artist
+            #print("Artist:",Artist) #Artist
             #global artist 
             artist = Artist
-        except KeyError:
-            print("no artist")
-        except NameError:
-            print("no artist")
+        except (KeyError, MutagenError, HeaderNotFoundError, ID3NoHeaderError, NameError, ID3Warning, mutagen.MutagenError, ID3NoHeaderError):
+            #print("no artist")
             pass
         try:
             Title = audio1['TIT2'].text[0]
             #global title
             title = Title             
-            print("Title:",audio1['TIT2'].text[0]) #title
+            #print("Title:",audio1['TIT2'].text[0]) #title
         except KeyError:
-            print("no title")
+            #print("no title")
             pass 
         try:
             Album = audio1['TALB'].text[0]
-            #global album
             album = Album
-            print("Album:",audio1['TALB'].text[0]) #album
-        except KeyError:
-            print("no album")
+            #print("Album:",audio1['TALB'].text[0]) #album
+        except (KeyError, NameError):
+            #print("no album")
             pass
         try:
             Genre = audio3.tag.genre.name
             #global genre
             genre = Genre
-            print("Genre:",audio3.tag.genre.name) #genre
-        except AttributeError:
-            print("no genre")
+            #print("Genre:",audio3.tag.genre.name) #genre
+        except (AttributeError, ID3Warning):
+            #print("no genre")
             pass
         try:
             Bpm = audio1['TBPM'].text[0]
             bpm = Bpm
-            print("BPM:",audio1['TBPM'].text[0]) #bpm
+            #print("BPM:",audio1['TBPM'].text[0]) #bpm
         except KeyError:
-            print("no bpm")
+            #print("no bpm")
             pass
         try:        
             Initial_Key = audio1['TKEY'].text[0]
             #global initial_key
             initial_key = Initial_Key
-            print("Initial Key:",audio1['TKEY'].text[0]) #initial key
+            #print("Initial Key:",audio1['TKEY'].text[0]) #initial key
         except KeyError:
-            print("no key")
+            #print("no key")
             pass
         try:        
             Date = audio1['TDRC'].text[0]
             #global date
             date = Date
-            print("Date:",audio1['TDRC'].text[0]) #date/year
+            #print("Date:",audio1['TDRC'].text[0]) #date/year
         except KeyError:
-            print("no date")
+            #print("no date")
             pass
 
-        song_length = (audio2.info.length) #length
-        def convert(seconds):
-            return time.strftime("%M:%S", time.gmtime(song_length))
-        print("Length",convert(song_length))
-        #global length
-        length = time.strftime("%M:%S", time.gmtime(song_length))
+        try:
+            song_length = (audio2.info.length) #length
+            def convert(seconds):
+                return time.strftime("%M:%S", time.gmtime(song_length))
+            #print("Length:",convert(song_length))
+            #global length
+            length = time.strftime("%M:%S", time.gmtime(song_length))
+        except (MutagenError, HeaderNotFoundError, InvalidMPEGHeader) as error:
+            #print(error, song)
+            pass
 
         Publisher = audio3.tag.publisher
-        print("Publisher:",audio3.tag.publisher) #publisher
+        #print("Publisher:",audio3.tag.publisher) #publisher
         #global publisher
         publisher = Publisher
 
         song_bitrate = str(audio4.info.bit_rate)
         song_bitrate = re.sub('[^0-9]', '', song_bitrate)
-        print("Bit rate:",song_bitrate, "kbps") #bitrate
+        #print("Bit rate:",song_bitrate, "kbps") #bitrate
         #global bitrate
         bitrate = song_bitrate
-        print("File Location:",file_location, '\n')
+        #print("File Location:",file_location, '\n')
        
-        #json_song_info = {
-        #"artist": artist,
-        #"title": title,
-        #"album": album,
-        #"genre": genre,
-        #"bpm": Bpm,
-        #"initial_key": initial_key,
-        #"date": date,
-        #"length": length,
-        #"publisher": publisher,
-        #"bitrate": bitrate,
-        #"song location": file_location
-        #}
-        #song_json = json.dumps(str(json_song_info))
-        #print(song_json)
+        json_song_info = {
+        "artist": artist,
+        "title": title,
+        "album": album,
+        "genre": genre,
+        "bpm": bpm,
+        "initial_key": initial_key,
+        "date": date,
+        "length": length,
+        "publisher": publisher,
+        "bitrate": bitrate,
+        "song location": file_location
+        }
+        song_json = json.dumps(str(json_song_info))
+        print(song_json, '\n')
 write_directory_to_variable()
 
 print("my program took", time.time() - start_time, "seconds to run")
